3b171c923cefeeae870eca99e703d112d4e3e126
novaeeken <novaeeken@Novas-MacBook-Pro.local>
2021-04-07 12:32:40 +0200

    Correct spelling mistake

--- functions-methods.js
+++ functions-methods.js
@@ -16,10 +16,9 @@
 // Schrijf een functie genaamd typeOfEmail, die een emailadres verwacht. De functie checkt of het emailadres een novi domein heeft (medewerker), een novi-education domein (student), of extern domein (zoals gmail of outlook)
 // ---- Verwachte uitkomsten:
 // typeOfEmail("n.eeken@novi-education.nl") geeft "Student"
-// typeOfEmail("t.mellink@novi.nçl") geeft geeft "Medewerker"
+// typeOfEmail("t.mellink@novi.nl") geeft geeft "Medewerker"
 // typeOfEmail("novi.nlaapjesk@outlook.com") geeft geeft "Extern" <-- deze moet het ook doen!
 // typeOfEmail("a.wiersma@outlook.com") geeft "Extern"
-
 
 
 
